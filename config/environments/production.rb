5c4312bd4529e5d1f1076b2a0f2d1154f55d6e8f
RicardoSe14 <Ricardo.s14@gmail.com>
2023-07-25 06:45:08 -0400

    sexta  subida con pagi

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,6 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.secret_key_base = 'blipblapblup'
   
   config.cache_classes = true
 
